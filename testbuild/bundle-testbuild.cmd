68d5fa4a6baf2c262e5cad749d3b09c288831391
adipose <3324395+adipose@users.noreply.github.com>
2019-07-24 18:00:34 -0700

    Fix test build and script

--- testbuild/bundle-testbuild.cmd
+++ testbuild/bundle-testbuild.cmd
@@ -2,9 +2,11 @@
 
 copy ..\bin\mpc-hc_x86\mpc-hc.exe .
 
-xcopy /e/i /y ..\bin\mpc-hc_x86\Shaders Shaders
-xcopy /e/i /y ..\bin\mpc-hc_x86\LAVFilters LAVFilters
-xcopy /e/i /y ..\bin\mpc-hc_x86\Lang Lang
+rd /s/y Shaders LAVFilters Lang
 
-7za a mpc-hc.7z mpc-hc.exe Shaders LAVFilters Lang
+robocopy "..\bin\mpc-hc_x86\Shaders" "Shaders" /S /XO /XF
+robocopy "..\bin\mpc-hc_x86\LAVFilters" "LAVFilters" /S /XO /XF "*.pdb"
+robocopy "..\bin\mpc-hc_x86\Lang" "Lang" /S /XO /XF
+
+7za a mpc-hc.7z mpc-hc.exe Shaders\* LAVFilters\* Lang\*
 pause
