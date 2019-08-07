3082c577b1b54f5031aa12a18bb618cbf173056a
adipose <3324395+adipose@users.noreply.github.com>
2019-08-07 12:56:09 -0700

    Test builds

--- testbuild/bundle-testbuild.cmd
+++ testbuild/bundle-testbuild.cmd
@@ -8,9 +8,9 @@ copy ..\bin\mpc-hc_x86\mpc-hc.exe .
 
 rd /s/y Shaders LAVFilters Lang
 
-robocopy "..\src\mpc-hc\res\shaders\external" "Shaders" /S /XO /XF
+robocopy "..\src\mpc-hc\res\shaders\external" "Shaders" /S /XO
 robocopy "..\bin\mpc-hc_x86\LAVFilters" "LAVFilters" /S /XO /XF "*.pdb"
-robocopy "..\bin\mpc-hc_x86\Lang" "Lang" /S /XO /XF
+robocopy "..\bin\mpc-hc_x86\Lang" "Lang" /S /XO
 
 7za a mpc-hc.7z mpc-hc.exe Shaders\* LAVFilters\* Lang\*
 pause
--- testbuild64/bundle-testbuild.cmd
+++ testbuild64/bundle-testbuild.cmd
@@ -8,9 +8,9 @@ copy ..\bin\mpc-hc_x64\mpc-hc64.exe .
 
 rd /s/y Shaders LAVFilters64 Lang
 
-robocopy "..\src\mpc-hc\res\shaders\external" "Shaders" /S /XO /XF
+robocopy "..\src\mpc-hc\res\shaders\external" "Shaders" /S /XO
 robocopy "..\bin\mpc-hc_x64\LAVFilters64" "LAVFilters64" /S /XO /XF "*.pdb"
-robocopy "..\bin\mpc-hc_x64\Lang" "Lang" /S /XO /XF
+robocopy "..\bin\mpc-hc_x64\Lang" "Lang" /S /XO
 
 7za a mpc-hc64.7z mpc-hc64.exe Shaders\* LAVFilters64\* Lang\*
 pause
